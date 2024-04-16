37205d05f51212b041d35f8110134e3ae4e41825
LifeIsOne <didtmdgh258@gmail.com>
2024-04-16 11:06:32 +0900

    코드 완성

--- lib/main.dart
+++ lib/main.dart
@@ -92,14 +92,18 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
   }
 
   Widget _buildHeaderSelector() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        _buildHeaderSelectorButton(0, Icons.directions_bike),
-        _buildHeaderSelectorButton(1, Icons.motorcycle),
-        _buildHeaderSelectorButton(2, CupertinoIcons.car_detailed),
-        _buildHeaderSelectorButton(3, CupertinoIcons.airplane),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(
+          left: 30.0, right: 30.0, top: 14.0, bottom: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildHeaderSelectorButton(0, Icons.directions_bike),
+          _buildHeaderSelectorButton(1, Icons.motorcycle),
+          _buildHeaderSelectorButton(2, CupertinoIcons.car_detailed),
+          _buildHeaderSelectorButton(3, CupertinoIcons.airplane),
+        ],
+      ),
     );
   }
 
@@ -130,30 +134,133 @@ class ShoppingCartDetail extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(40), // 끝을 둥글게 40 - 뭐를?
       ),
-      child: Column(
-        children: [
-          _buildDetailNameAndPrice(),
-          _buildDetailRatingAndReviewCount(),
-          _buildDetailColorOptions(),
-          _buildDetailButton(context),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingAndReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(context),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildDetailNameAndPrice() {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Text를 양 끝으로
+        children: [
+          Text(
+            "Urban Soft Al 10.0",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "\$699",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailRatingAndReviewCount() {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        children: [
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Icon(Icons.star, color: Colors.yellow),
+          Spacer(),
+          Text("review "),
+          Text("(26)", style: TextStyle(color: Colors.blue))
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailColorOptions() {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Color Options"),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              // 함수 사용 -> 동일한 색상 아이콘 재사용
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailIcon(Color mColor) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10.0),
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Positioned(
+            left: 5,
+            top: 5,
+            child: ClipOval(
+              child: Container(
+                color: mColor,
+                width: 40,
+                height: 40,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Widget _buildDetailButton(BuildContext context) {
-    return SizedBox();
+    return Align(
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: kAccentColor,
+          minimumSize: Size(300, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(
+          "Add to Cart",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
